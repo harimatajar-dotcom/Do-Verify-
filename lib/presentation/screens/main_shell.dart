d3d498106cde1e5b4d06d875b1305ef288281456
Chathan <chathan@doverify.com>
2026-01-29 16:16:30 +0530

    WIP: Add MainShell for fixed bottom nav and IndexedStack navigation

---
+++ lib/presentation/screens/main_shell.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
+import '../widgets/common/bottom_nav_bar.dart';
+
+// Import content screens
+import 'home_screen.dart';
+import 'templates_screen.dart';
+import 'create_screen.dart';
+import 'shared_screen.dart';
+import 'profile_screen.dart';
+
+/// Main navigation shell with persistent bottom nav bar
+/// Uses IndexedStack to keep pages in memory and prevent refreshing
+class MainShell extends StatefulWidget {
+  final int initialIndex;
+  
+  const MainShell({super.key, this.initialIndex = 0});
+
+  @override
+  State<MainShell> createState() => _MainShellState();
+}
+
+class _MainShellState extends State<MainShell> {
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+  }
+
+  void _onNavTap(int index) {
+    if (index == _currentIndex) return;
+    setState(() => _currentIndex = index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      backgroundColor: isDark ? AppColors.darkBackground : AppColors.lightBackground,
+      resizeToAvoidBottomInset: false, // Prevents bottom nav from moving with keyboard
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          HomeContent(),
+          TemplatesContent(),
+          CreateContent(),
+          SharedContent(),
+          ProfileContent(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: _onNavTap,
+      ),
+    );
+  }
+}
